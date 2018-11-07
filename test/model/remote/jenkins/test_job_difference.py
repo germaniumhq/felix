f4bdf79000ed9c0797c4bad6ea2873457d06427e
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-11-07 18:33:34 +0100

    Branch differences seem to show notifications.

--- test/model/remote/jenkins/test_job_difference.py
+++ test/model/remote/jenkins/test_job_difference.py
@@ -17,7 +17,8 @@ def compare_branches(initial_branches: List[JenkinsJobBranch],
     for branch in updated_branches:
         if not find_branch(initial_branches, branch):
             if branch.status == BuildStatus.SUCCESS or branch.status == BuildStatus.FAILED:
-                notifications += Notification()
+                last_build = get_last_finished_build(branch)
+                notifications.append(Notification(branch, last_build))
 
     for initial_branch in initial_branches:
         updated_branch = find_branch(updated_branches, initial_branch)
@@ -27,9 +28,8 @@ def compare_branches(initial_branches: List[JenkinsJobBranch],
 
         if len(updated_branch.builds) != len(initial_branch.builds):
             last_build = get_last_finished_build(updated_branch)
-            print(last_build._mopyx_target)
-            if last_build.status == BuildStatus.SUCCESS or last_build.status == BuildStatus.FAILED:
-                notifications += Notification()  # FIXME: state changes only?
+            if last_build.status == BuildStatus.SUCCESS or last_build.status == BuildStatus.FAILURE:
+                notifications.append(Notification(updated_branch, last_build))  # FIXME: state changes only?
 
     return notifications
 
@@ -45,7 +45,7 @@ def find_branch(branch_list: List[JenkinsJobBranch],
 
 
 def get_last_finished_build(branch: JenkinsJobBranch) -> JenkinsJobBranchBuild:
-    return branch.builds[-1]
+    return branch.builds[0]
 
 
 class TestJobDifferencesInBuild(unittest.TestCase):
@@ -64,6 +64,12 @@ class TestJobDifferencesInBuild(unittest.TestCase):
         rerun_branches = read_build_job_branches("jd", branch_rerun)
 
         notifications = compare_branches(initial_branches, rerun_branches)
+
+        self.assertEqual(1, len(notifications))
+        self.assertEqual("jd", notifications[0].branch.project_name)
+        self.assertEqual("feature/wut", notifications[0].branch.decoded_branch_name)
+        self.assertEqual("#2", notifications[0].build.name)
+
 
 if __name__ == '__main__':
     unittest.main()
